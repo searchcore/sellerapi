c4ffa613bccfbe6b8750829e2c679f95ffca0c42
searchcore <90134005+searchcore@users.noreply.github.com>
2026-02-09 14:55:32 +0300

    Added error response factory

--- src/core/responses.py
+++ src/core/responses.py
@@ -1,6 +1,7 @@
-from typing import TypeVar, Generic
-from pydantic import BaseModel
+from typing import TypeVar, Generic, Any, Literal, Type
+from pydantic import BaseModel, create_model
 
+from src.core.exceptions import BaseAppException
 
 T = TypeVar("T")
 
@@ -9,8 +10,27 @@ class SuccessResponse(BaseModel, Generic[T]):
     data: T
 
 
-class ErrorResponse(BaseModel):
-    error: str
+class ErrorResponse(BaseModel, Generic[T]):
+    error: T
+
+
+# proper way would be manual responses writing
+# but for now its a bit tedious
+# main issues with such mapper are:
+# - unintentional changes in docs when changing inner application error
+# - poor openapi naming like ErrorResponse_PurchaseTokenNotFoundResponse_
+# - even more poor naming when exceptions have same names (pydantic adds extra info to classname)
+# but for now i think its ok
+def create_error_resp(exc_cls: Type[BaseAppException]) -> Type[ErrorResponse]:
+    name = f"{exc_cls.__name__}Response"
+
+    Error = create_model(
+        name,
+        code=(Literal[exc_cls.code], exc_cls.code),
+        details=(str, ...),
+        params=(dict[str, Any], {})
+    )
+    return ErrorResponse[Error]
 
 
 def success(response: T) -> SuccessResponse[T]:
